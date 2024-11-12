22e595a6d50121e04817eea661812ff2cafb68f5
Primo malvado <rui.fajozes@gmail.com>
2024-11-12 12:10:50 +0000

    ds

--- source/lib.asm
+++ source/lib.asm
@@ -1,36 +1,6 @@
 
  
 
-OLD_getPixelAddress:
-    ld hl, row_memory
-
-    ld a, l
-    add b
-    _if_not NC
-        inc h
-    _end_if
-    add b
-
-    _if_not NC
-        inc h
-    _end_if
-    ld l, a
-
-
-    push bc
-    ld c,(hl)
-    inc hl
-    ld b,(hl)
-    ld l,c
-    ld h,b	
-    pop bc
-
-
-    ld a, l
-    add c
-    ld l, a
-    
-    ret
 getPixelAddress:
     ld de, row_memory
 
